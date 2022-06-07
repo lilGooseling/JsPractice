a3cde70c926281b403785c0239b5108b2353e811
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-07 22:49:54 +0300

    Типы данных в Js

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,41 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+//const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+//number = 10;
+//console.log(number);
+
+//const obj = {
+    //a: 50
+//};
+
+//obj.a = 10;
+
+//console.log(obj);
+
+const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000;
+
+console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длинна: ' + vehicleBodyLength);
+
+let number = 4.6;
+console.log(4/0);
+
+const person = "Alex";
+
+const bool = true;
+
+let und;
+console.log(und);
 
 const obj = {
-    a: 50
+    name: "John",
+    age: 25,
+    isMarried: false
 };
+//console.log(obj.name);
+console.log(obj['name']);
 
-obj.a = 10;
-
-console.log(obj);
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}];
+console.log(arr[1]);
