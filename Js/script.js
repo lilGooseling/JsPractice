0a5c4cc85982c030a61dd99d59600e215105640b
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-08 22:34:58 +0300

    Разница между объектами и массивами

--- Js/script.js
+++ Js/script.js
@@ -14,28 +14,49 @@
 
 //console.log(obj);
 
-const vehicleBodyWidth = 5000;
-const vehicleBodyLength = 4000;
+//const vehicleBodyWidth = 5000;
+//const vehicleBodyLength = 4000;
 
-console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длинна: ' + vehicleBodyLength);
+//console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длинна: ' + vehicleBodyLength);
 
-let number = 4.6;
-console.log(4/0);
+//let number = 4.6;
+//console.log(4/0);
 
-const person = "Alex";
+//const person = "Alex";
 
-const bool = true;
+//const bool = true;
 
-let und;
-console.log(und);
+//let und;
+//console.log(und);
 
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+//const obj = {
+    //name: "John",
+    //age: 25,
+    //isMarried: false
+//};
 //console.log(obj.name);
-console.log(obj['name']);
+//console.log(obj['name']);
 
-let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}];
+//let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}];
+//console.log(arr[1]);
+
+const arr = ['a', 'b', 'c'];
 console.log(arr[1]);
+
+const arrObg = {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+};
+//arrObg.d = '12345';
+
+arrObg['d'] = '12345';
+
+console.log(arrObg['d']);
+console.log(arrObg.d);
+
+console.log(arrObg[1]);
+
+
+
+const obj = {a: 1, b: 2};
