9ea9eef30c252693ef6e0faa38245bcc14268115
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-09 23:17:30 +0300

    Общение с пользователем

--- Js/script.js
+++ Js/script.js
@@ -40,23 +40,36 @@
 //let arr = ['plum.png', 'orange.jpg', 6, 'apple.png', {}];
 //console.log(arr[1]);
 
-const arr = ['a', 'b', 'c'];
-console.log(arr[1]);
+//const arr = ['a', 'b', 'c'];
+//console.log(arr[1]);
 
-const arrObg = {
-    0: 'a',
-    1: 'b',
-    2: 'c'
-};
+//const arrObg = {
+    //0: 'a',
+    //1: 'b',
+    //2: 'c'
+//};
 //arrObg.d = '12345';
 
-arrObg['d'] = '12345';
+//arrObg['d'] = '12345';
 
-console.log(arrObg['d']);
-console.log(arrObg.d);
+//console.log(arrObg['d']);
+//console.log(arrObg.d);
 
-console.log(arrObg[1]);
+//console.log(arrObg[1]);
 
+//const obj = {a: 1, b: 2};
 
+//alert('Hello');
 
-const obj = {a: 1, b: 2};
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('Вам есть 18?', '');
+//console.log(answer + 5);
+
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+//document.write(answers);
+console.log(typeof (answers));
