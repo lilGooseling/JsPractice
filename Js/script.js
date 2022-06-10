e573d24bdc59e9cd3e0ae2c2ef32d0bccd4782ce
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-10 22:33:34 +0300

    Интерполяция

--- Js/script.js
+++ Js/script.js
@@ -67,9 +67,16 @@
 //const answer = +prompt('Вам есть 18?', '');
 //console.log(answer + 5);
 
-const answers = [];
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+//const answers = [];
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
 //document.write(answers);
-console.log(typeof (answers));
+//console.log(typeof (answers));
+
+const category = 'toys';
+//console.log('http://someurl.com/' + category);
+console.log(`http://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
