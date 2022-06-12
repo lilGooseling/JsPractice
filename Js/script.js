755eda62ca3d65bb45ace9f161f9fdfae3e7ebef
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-12 23:29:47 +0300

    Практика

--- Js/script.js
+++ Js/script.js
@@ -81,20 +81,20 @@
 //const user = 'Ivan';
 //alert(`Привет, ${user}`);
 
-console.log('arr' + '- object');
-console.log(4 + '- object');
-console.log(4 + +'5');
+//console.log('arr' + '- object');
+//console.log(4 + '- object');
+//console.log(4 + +'5');
 
-let incr = 10,
-    decr = 10;
+//let incr = 10,
+    //decr = 10;
 //incr++;
 //decr--;
-console.log(++incr);
-console.log(--decr);
+//console.log(++incr);
+//console.log(--decr);
 
-console.log(5%2);
+//console.log(5%2);
 
-console.log(2*4 === 8);
+//console.log(2*4 === 8);
 
 //const isChecked = true,
     //isClose = true;
@@ -104,6 +104,27 @@ console.log(2*4 === 8);
     //isClose = false;
 //console.log(isChecked || isClose);
 
-const isChecked = false,
-    isClose = false;
-console.log(isChecked || !isClose);
+//const isChecked = false,
+    //isClose = false;
+//console.log(isChecked || !isClose);
+
+
+
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
