f10ed91a462e625823e3cb37e18eaa38c93fe398
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-14 22:56:42 +0300

    Условия

--- Js/script.js
+++ Js/script.js
@@ -110,21 +110,53 @@
 
 
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+//const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+//const personalMovieDB = {
+    //count: numberOfFilms,
+    //movies: {},
+    //actors: {},
+    //genres: [],
+    //privat: false
+//};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+//const a = prompt('Один из последних просмотренных фильмов?', ''),
+     //b = prompt('На сколько оцените его?', ''),
+      //c = prompt('Один из последних просмотренных фильмов?', ''),
+      //d = prompt('На сколько оцените его?', '');
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
+//console.log(personalMovieDB);
+
+//if (4 == 9) {
+    //console.log('Ok!');
+//} else {
+    //console.log('Error');
+//}
+
+const num = 50;
+//if (num < 49) {
+    //console.log('Error');
+//} else if (num > 100) {
+    //console.log('too much');
+//} else {
+    //console.log('Ok!');
+//}
+
+//(num === 50) ? console.log('Ok!') : console.log('Error');
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Верно!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
