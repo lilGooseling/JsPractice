8412113fab0d757ea6a833cf9365912a739463a5
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-17 22:16:19 +0300

    Циклы

--- Js/script.js
+++ Js/script.js
@@ -271,5 +271,37 @@ if (broccoli && water || potato === 3 && meat) {
     console.log('Done!')
 }
 
+let num = 50;
+//while (num <= 55) {
+    //console.log(num);
+    //num++;
+//}
 
+//do {
+    //console.log(num);
+    //num++;
+//}
+//while (num < 55);
 
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i ===6){
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i ===6){
+        continue;
+    }
+    console.log(i);
+}
