97270d9c2e1a57180c5fadec0eeb85079f04f851
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-18 22:52:05 +0300

    Вложенность циклов

--- Js/script.js
+++ Js/script.js
@@ -217,6 +217,7 @@
     //console.log('Мы ушли');
 //}
 
+/*
 //let johnReport, alexReport, samReport, mariaReport = 'done';
 //console.log( johnReport || alexReport || samReport || mariaReport);
 
@@ -305,3 +306,23 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+*/
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i ++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    result += '\n';
+}
+
+console.log(result);
