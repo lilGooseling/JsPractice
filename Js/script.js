32c6b5dfeb89538ee068c98264972b99717b4414
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-19 21:09:53 +0300

    Метки

--- Js/script.js
+++ Js/script.js
@@ -315,14 +315,27 @@ for (let i = 0; i < 3; i++) {
 }
 */
 
-let result = '';
-const length = 7;
+//let result = '';
+//const length = 7;
 
-for (let i = 1; i < length; i ++) {
-    for (let j = 0; j < i; j++) {
-        result += '*';
+//for (let i = 1; i < length; i ++) {
+    //for (let j = 0; j < i; j++) {
+        //result += '*';
+    //}
+    //result += '\n';
+//}
+
+//console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level : ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level : ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
     }
-    result += '\n';
 }
 
-console.log(result);
+
