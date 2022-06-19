e2a22cc768ebaec98b8978d7ea431a1cbdf1be40
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-19 22:21:37 +0300

    Практика 2

--- Js/script.js
+++ Js/script.js
@@ -327,15 +327,57 @@ for (let i = 0; i < 3; i++) {
 
 //console.log(result);
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level : ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level : ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`Third level: ${k}`);
-        }
+//first: for (let i = 0; i < 3; i++) {
+    //console.log(`First level : ${i}`);
+    //for (let j = 0; j < 3; j++) {
+        //console.log(`Second level : ${j}`);
+        //for (let k = 0; k < 3; k++) {
+            //if (k === 2) continue first;
+            //console.log(`Third level: ${k}`);
+        //}
+    //}
+//}
+
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+/*const a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('Один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);*/
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    if ( a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('error');
+        i--;
     }
 }
 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else  if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >=30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
+
+console.log(personalMovieDB);
