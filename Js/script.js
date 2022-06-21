43d31ecf13caf431d3a9afcb2ce6f8769697272b
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-21 21:45:43 +0300

    Аргументы функций

--- Js/script.js
+++ Js/script.js
@@ -410,8 +410,17 @@ console.log(calc(10, 6));*/
 const anotherNum = ret();
 console.log(anotherNum);*/
 
-const logger = function () {
+/*const logger = function () {
     console.log('Hello');
 };
-logger();
+logger();*/
+
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr) {
+    console.log(amount * curr);
+}
+convert(500, usdCurr);
+convert(500, eurCurr);
 
