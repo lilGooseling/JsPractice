b369246084fe92f7495f185c6379a679a0128a91
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-26 22:51:46 +0300

    Методы и свойства строк и чисел, 2

--- Js/script.js
+++ Js/script.js
@@ -463,5 +463,13 @@ console.log(fruit.indexOf('q'));
 
 const logg = 'Hello world';
 console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
 
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
 
