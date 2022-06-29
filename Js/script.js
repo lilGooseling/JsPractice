d3d7e0a8a3da453d949ef6e71cfb5ad6b745e363
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-29 21:26:15 +0300

    Метод trim()

--- Js/script.js
+++ Js/script.js
@@ -507,7 +507,7 @@ console.log(personalMovieDB);*/
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
             b = prompt('На сколько оцените его?', '');
         if ( a != null && b != null && a !== '' && b !== '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
