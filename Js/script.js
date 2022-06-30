f15e0952e66a084ed8c813509f3701bb0ee7a14f
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-30 22:18:59 +0300

    Методы в объекте

--- Js/script.js
+++ Js/script.js
@@ -596,18 +596,30 @@ const options = {
     colors: {
         border: 'black',
         bg: 'red'
+    },
+    makeTest: function () {
+        console.log('Test');
     }
 };
+options.makeTest();
+
+
+console.log(Object.keys(options).length);
+
 //console.log(options.name);
 //delete options.name;
 //console.log(options);
 
+/*let counter = 0;
 for (let key in options) {
     if (typeof (options[key]) === 'object') {
         for (let i in options[key]) {
             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
         }
     } else {
         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
     }
 }
+console.log(counter);*/
