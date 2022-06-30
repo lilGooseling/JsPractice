0c2278a8ef034d3071c5ca34109b73a942755822
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-06-30 21:43:40 +0300

    Перебор свойств в объекте

--- Js/script.js
+++ Js/script.js
@@ -563,7 +563,7 @@ writeYourGenres();*/
 
 
 
-function first() {
+/*function first() {
     //Do something
     setTimeout(function () {
         console.log(1);
@@ -586,7 +586,28 @@ function done() {
     console.log('Я прошла урок!');
 }
 
-learnJS('JavaScript', done);
+learnJS('JavaScript', done);*/
 
 
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+//console.log(options.name);
+//delete options.name;
+//console.log(options);
 
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
