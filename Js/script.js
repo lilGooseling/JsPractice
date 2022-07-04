84db9db29015250c43c947232273ad03a01801bd
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-04 21:58:13 +0300

    Методы перебора в массиве

--- Js/script.js
+++ Js/script.js
@@ -629,7 +629,7 @@ for (let key in options) {
 console.log(counter);*/
 
 
-const arr = [2, 3, 6, 8, 10];
+//const arr = [2, 3, 6, 8, 10];
 //arr.pop();
 //arr.push(10);
 //console.log(arr);
@@ -640,7 +640,19 @@ const arr = [2, 3, 6, 8, 10];
     console.log(value);
 }*/
 
-arr.forEach(function (item, i, arr) {
+/*arr.forEach(function (item, i, arr) {
     console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+});*/
+
+//const str = prompt('', '');
+//const products = str.split(', ');
+//products.sort();
+//console.log(products.join('; '));
+
+const arr = [2, 13, 26, 8, 10];
+arr.sort(compareNum);
+console.log(arr);
+function compareNum(a, b) {
+    return a - b;
+}
 
