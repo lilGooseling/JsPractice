f49472af0990719c3a2312638a28b354714e28f8
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-05 22:01:40 +0300

    Передача по ссылке или по значению, 1

--- Js/script.js
+++ Js/script.js
@@ -656,3 +656,41 @@ function compareNum(a, b) {
     return a - b;
 }
 
+let a = 5,
+    b = a;
+b = b + 5;
+console.log(b);
+console.log(a);
+
+
+const obj = {
+    a: 5,
+    b: 1
+};
+/*const copy = obj;
+copy.a = 10;
+console.log(copy);
+console.log(obj);*/
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
