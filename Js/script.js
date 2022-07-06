23fe654429a6031a797f0a310ec9df6930d7b86e
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-06 22:19:10 +0300

    Передача по ссылке или по значению, 2

--- Js/script.js
+++ Js/script.js
@@ -649,7 +649,7 @@ console.log(counter);*/
 //products.sort();
 //console.log(products.join('; '));
 
-const arr = [2, 13, 26, 8, 10];
+/*const arr = [2, 13, 26, 8, 10];
 arr.sort(compareNum);
 console.log(arr);
 function compareNum(a, b) {
@@ -666,13 +666,13 @@ console.log(a);
 const obj = {
     a: 5,
     b: 1
-};
+};*/
 /*const copy = obj;
 copy.a = 10;
 console.log(copy);
 console.log(obj);*/
 
-function copy(mainObj) {
+/*function copy(mainObj) {
     let objCopy = {};
 
     let key;
@@ -692,5 +692,46 @@ const numbers = {
 };
 const newNumbers = copy(numbers);
 newNumbers.a = 10;
-console.log(newNumbers);
-console.log(numbers);
+//console.log(newNumbers);
+//console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+clone.d = 20;
+console.log(add);
+console.log(clone);*/
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'butterfly';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+console.log(newAarray);
+
+const q = {
+    one: 1,
+    two: 2
+};
+const newObj = {...q};
+console.log(newObj);
