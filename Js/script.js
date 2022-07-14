5fb914aa5141f0fac3e2ba24da8cc1a37c6295af
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-14 23:22:35 +0300

    Замыкание и лексическое окружение

--- Js/script.js
+++ Js/script.js
@@ -874,7 +874,7 @@ function compareNum(a, b) {
 console.log(sorted);*/
 
 
-console.log(typeof (String(null)));
+/*console.log(typeof (String(null)));
 console.log(typeof (5 + ''));
 
 const num = 5;
@@ -897,6 +897,36 @@ if (switcher) {
 }
 
 console.log(typeof (Boolean('4')));
-console.log(typeof (!!'12345'));
+console.log(typeof (!!'12345'));*/
+
+
+/*let number = 5; debugger
+function logNumber() {
+    //let number = 4; debugger
+    console.log(number);debugger
+}
+
+number = 6;
+logNumber();debugger
+
+number = 8;
+logNumber();debugger*/
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function () {debugger
+        counter = counter + 1;debugger
+        return counter;debugger
+    };
+
+    return myFunction;
+}
+debugger
+const increment = createCounter();debugger
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
+console.log(c1, c2, c3);
 
 
