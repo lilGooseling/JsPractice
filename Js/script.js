6633783d4da091778ff57e914aefe21f21cc17dc
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-18 22:05:17 +0300

    Вопросы, часть 2

--- Js/script.js
+++ Js/script.js
@@ -986,4 +986,21 @@ alert(x);*/
 // [] + 1 + 2
 //console.log([] + 1 + 2);
 
-alert('1'[0]);
+//alert('1'[0]);
+
+//console.log( 2 && 1 && null && 0 && undefined);
+
+//console.log( !!( 1 && 2) === (1 && 2));
+
+//alert( null || 2 && 3 || 4);
+
+/*const a = [1,2,3];
+const b = [1,2,3];
+console.log( a == b);*/
+
+//alert( + 'infinity');
+
+//console.log( 'Ёжик' > 'Яблоко');
+
+console.log(0 || '' || 2 || undefined || true || false);
+
