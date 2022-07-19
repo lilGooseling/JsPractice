d03e601810d704f58bcd8f8aad6939b20bd74e1e
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-19 22:21:22 +0300

    Получение элементов со страницы, часть 2

--- Js/script.js
+++ Js/script.js
@@ -1007,3 +1007,18 @@ console.log( a == b);*/
 
 const box = document.getElementById('box');
 console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
