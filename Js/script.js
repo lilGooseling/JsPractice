1424e0c7cca43e43ff49f3a84e5ef498afe09f38
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-27 22:41:53 +0300

    Метод EventTarget.addEventListener()

--- Js/script.js
+++ Js/script.js
@@ -1075,7 +1075,8 @@ wrapper.append(div);
 //div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');*/
 
 
-const btn = document.querySelector('button');
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
 
 /*btn.onclick = function () {
     alert('Click');
@@ -1087,8 +1088,34 @@ const btn = document.querySelector('button');
 /*btn.addEventListener('click', () => {
     alert(' Second click');
 });*/
-btn.addEventListener('click', (e) => {
+
+
+//let i = 0;
+const deleteElement = (e) => {
+    //e.target.remove();
+    console.log(e.currentTarget);
+    console.log(e.type);
+    //i++;
+   // if (i === 1) {
+        //btn.removeEventListener('click', deleteElement);
+    //}
+};
+/*btn.addEventListener('click', (e) => {
     console.log(e.target);
     e.target.remove();
     //console.log('Hover');
+});*/
+
+//btn.addEventListener('click', deleteElement);
+//overlay.addEventListener('click', deleteElement);
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', deleteElement, {once: true});
 });
+
+const link = document.querySelector('a');
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+});
+
