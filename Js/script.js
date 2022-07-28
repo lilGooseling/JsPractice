ceaea338b004f687f846ea2d66da0e4a0f60f43c
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-28 21:33:12 +0300

    Навигация по DOM

--- Js/script.js
+++ Js/script.js
@@ -1075,8 +1075,8 @@ wrapper.append(div);
 //div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');*/
 
 
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+//const btns = document.querySelectorAll('button'),
+      //overlay = document.querySelector('.overlay');
 
 /*btn.onclick = function () {
     alert('Click');
@@ -1091,7 +1091,7 @@ const btns = document.querySelectorAll('button'),
 
 
 //let i = 0;
-const deleteElement = (e) => {
+/*const deleteElement = (e) => {
     //e.target.remove();
     console.log(e.currentTarget);
     console.log(e.type);
@@ -1099,7 +1099,7 @@ const deleteElement = (e) => {
    // if (i === 1) {
         //btn.removeEventListener('click', deleteElement);
     //}
-};
+};*/
 /*btn.addEventListener('click', (e) => {
     console.log(e.target);
     e.target.remove();
@@ -1109,7 +1109,7 @@ const deleteElement = (e) => {
 //btn.addEventListener('click', deleteElement);
 //overlay.addEventListener('click', deleteElement);
 
-btns.forEach((btn) => {
+/*btns.forEach((btn) => {
     btn.addEventListener('click', deleteElement, {once: true});
 });
 
@@ -1117,5 +1117,30 @@ const link = document.querySelector('a');
 link.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event.target);
-});
+});*/
+
+//console.log(document.body);
+//console.log(document.body);
+//console.log(document.documentElement);
+//console.log(document.body.childNodes);
+
+//console.log(document.body.firstChild);
+//console.log(document.body.lastChild);
+//console.log(document.body.firstElementChild);
+//console.log(document.body.lastElementChild);
+
+
+//console.log(document.querySelector('#current').parentNode);
+//console.log(document.querySelector('#current').parentNode.parentNode);
+//console.log(document.querySelector('#current').parentElement);
+//console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName === '#text') {
+        continue;
+    }
+    console.log(node);
+}
+
+
 
