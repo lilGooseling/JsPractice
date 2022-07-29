e0c98c52493a9721780a6b8c700a44e5ce5f9230
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-29 22:42:05 +0300

    Рекурсия, часть 1

--- Js/script.js
+++ Js/script.js
@@ -1135,12 +1135,36 @@ link.addEventListener('click', (event) => {
 //console.log(document.querySelector('#current').parentElement);
 //console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-for (let node of document.body.childNodes) {
+/*for (let node of document.body.childNodes) {
     if (node.nodeName === '#text') {
         continue;
     }
     console.log(node);
+}*/
+
+
+pow(2, 1);
+pow(2, 2);
+pow(2, 3);
+pow(2,4);
+
+/*function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+    return result;
+}*/
+
+function pow(x, n) {
+    if (n===1) {
+        return x;
+    } else {
+        return x * pow( x, n - 1);
+    }
 }
+
+
 
 
 
