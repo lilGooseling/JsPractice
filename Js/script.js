601ddab10c4b381599ba2013d3664e066a4bbb61
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-07-30 22:46:44 +0300

    Рекурсия, часть 2

--- Js/script.js
+++ Js/script.js
@@ -1143,10 +1143,10 @@ link.addEventListener('click', (event) => {
 }*/
 
 
-pow(2, 1);
+/*pow(2, 1);
 pow(2, 2);
 pow(2, 3);
-pow(2,4);
+pow(2,4);*/
 
 /*function pow(x, n) {
     let result = 1;
@@ -1156,15 +1156,65 @@ pow(2,4);
     return result;
 }*/
 
-function pow(x, n) {
+/*function pow(x, n) {
     if (n===1) {
         return x;
     } else {
         return x * pow( x, n - 1);
     }
+}*/
+
+
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
+
+function getTotalProgressByIteration(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
 }
-
-
+console.log(getTotalProgressByIteration(students));
 
 
 
