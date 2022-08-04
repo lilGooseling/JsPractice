1d6f02b3ad3d79d02bae69935cdc73d704e12117
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-04 22:22:29 +0300

    События на мобильных устройствах

--- Js/script.js
+++ Js/script.js
@@ -1247,4 +1247,29 @@ const result = getTotalProgressByRecursion(students);
 console.log(result[0]/result[1]);
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start');
+        //console.log(e.touches);
+        //console.log(e.targetTouches);
+        console.log(e.changedTouches);
+    });
+
+
+    /*box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log('Move');
+    });*/
+
+    /*box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('End');
+    });*/
+
+});
+
+
 
