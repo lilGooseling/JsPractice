23d7c8ceba05a9e36b31dc4d145d60d488d6457b
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-07 22:59:21 +0300

    Оператор опциональной цепочки

--- Js/script.js
+++ Js/script.js
@@ -1279,7 +1279,7 @@ console.log(result[0]/result[1]);
 
 
 
-const box = document.querySelector('.box2');
+/*const box = document.querySelector('.box2');
 const newHeight = 100;
 const newWidth = 400;
 
@@ -1294,5 +1294,33 @@ changeParams(box, newHeight, newWidth);
 
 let userName;
 let userKey;
-console.log(userName ?? userKey ?? 'User');
+console.log(userName ?? userKey ?? 'User');*/
+
+const box = document.querySelector('.box2');
+const block = document.querySelector('.block');
+
+console.log(block);
+
+/*if (block) {
+    console.log(block.textContent);
+}*/
+
+//console.log(block?.textContent);
+//console.log(1 + 2);
+
+const userData = {
+    name: 'Ivan',
+    age: null,
+    say: function () {
+        console.log('Hello');
+    }
+};
+
+userData.say();
+userData.hey?.();
+/*if ( userData && userData.skills && userData.skills.js) {
+    console.log(userData.skills.js);
+}*/
+console.log(userData.skills?.js);
+
 
