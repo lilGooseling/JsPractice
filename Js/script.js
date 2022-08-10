9c444dc2bfe5291f9ea4b53f03d4cceb33e1eb34
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-10 22:57:24 +0300

    Дескрипторы свойств

--- Js/script.js
+++ Js/script.js
@@ -1378,3 +1378,18 @@ console.log(myAwesomeDb[Symbol.for('id')]);
 console.log(myAwesomeDb);
 
 
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    birthday: '20/04/1993',
+    showMyPublicData: function () {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+console.log(Object.getOwnPropertyDescriptor(user, 'name'));
+Object.defineProperty(user, 'name', {writable: false});
+//user.name = 'joja';
+
+//writable
+//enumerable
+//configurable
