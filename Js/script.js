d6b1634d00ce842b5b4dd9fb917b3f0b99095f12
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-11 22:52:04 +0300

    Флаги. Методы объектов.

--- Js/script.js
+++ Js/script.js
@@ -1378,16 +1378,34 @@ console.log(myAwesomeDb[Symbol.for('id')]);
 console.log(myAwesomeDb);
 
 
+const birthday = Symbol('birthday');
+
 const user = {
     name: 'Alex',
     surname: 'Smith',
-    birthday: '20/04/1993',
+    //birthday: '20/04/1993',
+    [birthday]: '20/04/2022',
     showMyPublicData: function () {
         console.log(`${this.name} ${this.surname}`);
     }
 };
-console.log(Object.getOwnPropertyDescriptor(user, 'name'));
-Object.defineProperty(user, 'name', {writable: false});
+
+Object.defineProperty(user, 'showMyPublicData', {enumerable: false});
+for (let key in user) console.log(key);
+console.log(Object.getOwnPropertyDescriptor(user, birthday));
+
+Object.defineProperties(user, {
+    name: {writable: false},
+    surname: {writable: false}
+});
+
+
+//Object.defineProperty(user, 'birthday', {value: prompt('Date?'), enumerable: true, configurable: true});
+//console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));
+
+//Object.defineProperty(user, 'name', {writable: false});
+//Object.defineProperty(user, 'gender', {value: 'male'});
+//console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
 //user.name = 'joja';
 
 //writable
