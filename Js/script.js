40008a2f9d627ea7930e78a4fc52942454b2dcc1
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-08-13 22:26:19 +0300

    Итерируемые конструкции, 2

--- Js/script.js
+++ Js/script.js
@@ -1417,7 +1417,7 @@ Object.defineProperties(user, {
 
 
 
-const user = {
+/*const user = {
     name: 'Alex',
     surname: 'Smith',
     birthday: '20/04/1993',
@@ -1428,24 +1428,58 @@ const user = {
 
 for (const key in user) {
     console.log(user[key]);
-}
+}*/
 
 /*for (const key of user) {
     console.log(key);
 }*/
 
 
-const arr = ['b', 'a', 'c'];
+/*const arr = ['b', 'a', 'c'];
+Array.prototype.someMethod = function () {};
+
+console.dir(arr);
 
 for (const key in arr) {
-    console.log(arr[key]);
-}
-
-for (const key of arr) {
     console.log(key);
-}
+}*/
+
+/*for (const key of arr) {
+    console.log(key);
+}*/
 
 /*const str = 'string';
 for (const key in str) {
     console.log(str[key]);
 }*/
+
+
+
+const salaries = {
+    john: 500,
+    ivan: 1000,
+    ann: 5000,
+    sayHello: function () {
+        console.log('Hello')
+    }
+};
+
+salaries[Symbol.iterator] = function () {
+    return {
+        current: this.john,
+        last: this.ann,
+
+        next() {
+            if (this.current < this.last) {
+                this.current = this.current + 500;
+                return {done: false, value: this.current}
+            } else {
+                return {done: true}
+            }
+        }
+    }
+};
+
+for (let res of salaries) {
+    console.log(res);
+}
