188c649cf30a7a9aafed3783970189aa10fd2e83
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-09-05 22:20:29 +0300

    Animation

--- Js/script.js
+++ Js/script.js
@@ -1658,16 +1658,44 @@ const btn = document.querySelector('.btn');
 let timerId,
     i = 0;
 
+function myAnimation() {
+    const elem = document.querySelector('.box5');
+    let pos = 0;
 
-btn.addEventListener('click', () => {
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos === 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+
+
+
+/*btn.addEventListener('click', () => {
     timerId = setInterval(logger, 2000);
-});
+});*/
 
 
-function logger() {
+/*function logger() {
     if (i===3) {
         clearInterval(timerId);
     }
     console.log('text');
     i++;
-}
+}*/
+
+/*let id = setTimeout(function log() {
+    console.log('Hello');
+    id = setTimeout(log, 5500);
+}, 5500);*/
+
+
+
