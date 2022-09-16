b45a7eb194d38602e8eb744313c941d1a281a0ea
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-09-16 22:42:27 +0300

    Функции-конструкторы

--- Js/script.js
+++ Js/script.js
@@ -1847,7 +1847,7 @@ alert(`Цикл отработал за ${end - start} миллисекунд`);
 //window.scrollBy(x, y)
 //window.scrollTo(x, y)
 
-const box = document.querySelector('.box7');
+/*const box = document.querySelector('.box7');
 
 let observer = new MutationObserver(mutationRecords => {
     console.log(mutationRecords);
@@ -1857,4 +1857,32 @@ observer.observe(box, {
     childList: true
 });
 
-observer.disconnect();
+observer.disconnect();*/
+
+
+/*const num = new Number(3);
+console.log(num);*/
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function () {
+    console.log(`Пользователь ${this.name} вышел`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
