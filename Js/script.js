812dc1988f488ad460d35b2ca4d58adacba58bc3
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-10-14 11:57:54 +0300

    Инкапсуляция

--- Js/script.js
+++ Js/script.js
@@ -2335,7 +2335,7 @@ console.log(str.match(/\D/ig));*/
 //getters and setters
 
 
-const person = {
+/*const person = {
     name: 'Alex',
     age: 25,
 
@@ -2349,4 +2349,79 @@ const person = {
 };
 
 console.log(person.userAge = 30);
-console.log(person.userAge);
+console.log(person.userAge);*/
+
+
+
+// Инкапсуляция
+
+
+/*function User(name, age) {
+    this.name = name;
+    //this.age = age;
+    let userAge = age;
+
+    this.say = function () {
+        console.log(`Имя пользователя: ${this.name}, возраст пользователя: ${userAge}`);
+    };
+
+    this.getAge = function () {
+        return userAge;
+    };
+
+    this.setAge = function (age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            userAge = age;
+        } else {
+            console.log('Недопустимое значение');
+        }
+    };
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.name);
+console.log(ivan.getAge());
+
+ivan.setAge(30);
+ivan.setAge(300);
+console.log(ivan.getAge());
+//ivan.name = 'Alex';
+ivan.say();*/
+
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Ivanov';
+
+    say() {
+        console.log(`Имя пользователя: ${this.name}${this.#surname}, возраст пользователя: ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    get surname() {
+        return this.#surname
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение');
+        }
+    }
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.age); //getter
+ivan.age = 99; //setter
+console.log(ivan.age);
+ivan.say();
+
+console.log(ivan.surname);
