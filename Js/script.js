7da96d9294348165e294cf9bc8a2b72e8e607c21
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-10-26 22:11:27 +0300

    Микрозадачи и макрозадачи, событийный цикл

--- Js/script.js
+++ Js/script.js
@@ -2707,9 +2707,31 @@ function count() {
 }
 count();*/
 
-setTimeout(() => {
+/*setTimeout(() => {
     console.log(1);
 }, 0);
-console.log(2);
+console.log(2);*/
+
+
+setTimeout(() => console.log('timeout'));
+
+Promise.resolve()
+    .then(() => console.log('promise'));
+
+queueMicrotask(() => console.log('wow'));
+
+Promise.resolve()
+    .then(() => console.log('promise_2'));
+
+console.log('code');
+
+
+// () => {}
+// microtasks: then/catch/finally/await
+// render
+// () => {}
+// microtasks: then/catch/finally/await
+// render
+// () => {}
 
 
